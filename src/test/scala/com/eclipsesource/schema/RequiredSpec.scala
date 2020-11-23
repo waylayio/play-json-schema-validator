7e48bd2ae09e2211a6e89786cc63d769e80b5714
Bruno Ballekens <bruno@waylay.io>
2020-11-23 16:50:29 +0100

    fix: update instancePath in missing required property error to path to missing property add test

--- src/test/scala/com/eclipsesource/schema/RequiredSpec.scala
+++ src/test/scala/com/eclipsesource/schema/RequiredSpec.scala
@@ -3,6 +3,7 @@ package com.eclipsesource.schema
 import com.eclipsesource.schema.drafts.{Version4, Version7}
 import com.eclipsesource.schema.test.JsonSpec
 import org.specs2.mutable.Specification
+import play.api.libs.json.Json
 
 class RequiredSpec extends Specification with JsonSpec {
 
@@ -16,5 +17,25 @@ class RequiredSpec extends Specification with JsonSpec {
     import Version7._
     implicit val validator: SchemaValidator = SchemaValidator(Some(Version7))
     validate("required", "draft7")
+  }
+
+  "missing property error should contain instance path to missing property" in {
+    import Version4._
+    val validator = SchemaValidator(Some(Version4))
+    val recursiveSchema = JsonSource.schemaFromString(
+      """{
+        |  "properties": {
+        |    "foo": {}
+        |  },
+        |  "required": ["foo"]
+        |}""".stripMargin)
+
+    val instance = Json.obj("bar" -> false)
+
+    val result = validator.validate(recursiveSchema.get, instance)
+    result.asEither must beLeft.like {
+      case error => (error.toJson(0) \ "instancePath").get.as[String] == "/foo"
+    }
+    result.isError must beTrue
   }
 }
